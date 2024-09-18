22888ea77ae32facf8ef1345670d496638f12d3a
Flaviu Nemes <nemes.flaviu@gmail.com>
2024-09-18 15:46:58 +0300

    Added marketing deployment

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -12,7 +12,6 @@ export default () => {
 					<Switch>
 						<Route exact path='/pricing' component={Pricing} />
 						<Route path='/' component={Landing} />
-						<h2>hello</h2>
 					</Switch>
 				</BrowserRouter>
 			</StylesProvider>
