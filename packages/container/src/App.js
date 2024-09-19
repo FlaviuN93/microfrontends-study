dd4ead3fa48f00d96a873459b999055a1c635fde
Flaviu Nemes <nemes.flaviu@gmail.com>
2024-09-19 09:53:49 +0300

    Fixed the css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
 
+const generateClassName = createGenerateClassName({ productionPrefix: 'container' })
+
 export default () => {
 	return (
-		<BrowserRouter>
-			<div>
-				<Header />
-				<MarketingApp />
-				<h2>hello</h2>
-			</div>
-		</BrowserRouter>
+		<StylesProvider generateClassName={generateClassName}>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<MarketingApp />
+					<h2>hello</h2>
+				</div>
+			</BrowserRouter>
+		</StylesProvider>
 	)
 }
